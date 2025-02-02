4a062351481cc5140fff38a40a34cd2b7c63b908
ding-blip <ding.chl@northeastern.edu>
2025-02-02 17:04:08 -0500

    organized the components and added working links
    -routes working, placeholders created, and routes can be switched
    -organized folders
    -formatted intended navigation bar (there was a bug where the format wasn't working)

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -2,18 +2,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to Our Home Page</h1>
-                {}
-            <div className = 'button-container'>
-                <button> settings </button>
-                <button> tasks </button>
-                <button> events </button>
-            </div>
-        </div>
+        <h1 className = 'heading-style'>Welcome to Our Home Page</h1>
     )
 
-    
 }
 
 export default Home;
---
+++ src/components/tasks/index.tsx
@@ -0,0 +1,9 @@
+const Tasks = () => {
+    return (
+        <div>
+            <h1 className = 'heading-style'>This is our tasks page</h1>
+        </div>
+    )
+}
+
+export default Tasks;
