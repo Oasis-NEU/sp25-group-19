bd168531867be247eb313aa2cdc42dcdd778a64d
beckyzh3 <zheng.bec@northeastern.edu>
2025-02-02 13:13:06 -0500

    buttons correct

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import './index.css';
 const Home = () => {
     return (
-        <div>
+        <div className = "button-container"> 
             <button> settings </button>
             <button> tasks </button>
-            <button> events </button>
+            <button> events </button>    
         </div>
     )
+
+    
 }
 
 export default Home;
