0a529833763747339b524a139cb6fda168758c30
beckyzh3 <zheng.bec@northeastern.edu>
2025-02-02 12:38:17 -0500

    template

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import home from 'C:\Users\becky\Documents\sp25-group-19\src\components'
 import './App.css'
+import Home from './components/home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <home/>
+    <Home/>
   )
 }
 
