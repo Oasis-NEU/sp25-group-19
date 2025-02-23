0b63124e1038a0ebe487305d9e45d6ef610c3360
dunnmary <dunn.mar@northeastern.edu>
2025-02-23 13:56:22 -0500

    deleted textbox

--- src/components/tasks/index.tsx
+++ src/components/tasks/index.tsx
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 import supabase from '../supabaseClient.js';
+=======
+
+>>>>>>> Stashed changes
 
 const Tasks = () => {
 
@@ -18,8 +22,22 @@ const Tasks = () => {
     return (
         <div>
             <h1 className = 'heading-style'>This is our tasks page</h1>
+            <label form="Tasks">Tasks</label>
+            <textarea id="Tasks" name="Tasks" rows={10} cols={100}></textarea>
         </div>
     )
 }
 
+<<<<<<< Updated upstream
 export default Tasks;
+=======
+
+export default Tasks;
+
+// Types of data it will hold
+// String = Description of what the task is
+// String = When the task is due
+// Boolean = Repeat tasks. If false, no repeat. If true, repeat.
+// Boolean = remind (int) hours before event?
+// int = number of hours before event starts
+>>>>>>> Stashed changes
